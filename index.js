53609da89ed2a049495cd03226bae09678abd8eb
Paul Collas <paulcollas446@gmail.com>
2021-10-22 11:59:55 +0200

    UPDATE: create, read, update, delte post + code quality

--- index.js
+++ index.js
@@ -1,49 +1,163 @@
-const { ApolloServer, gql } = require('apollo-server');
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Post {
+    id: Int!
+    author: String!
+    content: String!
+    comments: String!
+  }
+
+
   type Query {
-    books: [Book]
+    users: [User]
+    posts: [Post]
   }
+
+  type Mutation {
+    createpost(id: Int, author: String, content: String, comments: String): Post!
+
+    readpostbyId(id: Int, author: String, content: String, comments: String): Post!
+
+    updatepostbyId(id: Int, author: String, content: String, comments: String): Post!
+
+    deletepostbyId(id: Int): Post!
+  }
+
 `;
 
-const books = [
+const users = [
     {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+      id: '1',
+      firstName: 'firstName1',
+      lastName: 'lastName1',
+      email: '1@1.com',
+      password: '1'
     },
     {
-      title: 'City of Glass',
-      author: 'Paul Auster',
+      id: '2',
+      firstName: 'firstName2',
+      lastName: 'lastName2',
+      email: '2@2.com',
+      password: '2'
     },
-];
+  ];
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
+  const posts = [
+    {
+      id: '6',
+      author: 'test',
+      content: 'truc',
+      comments: 'testttttt',
+    }
+  ]
+
 const resolvers = {
-    Query: {
-      books: () => books,
+  Query: {
+    users: () => users,
+    posts: () => posts,
+  },
+  Mutation: {
+
+
+    // 1.CREATE POST
+    createpost: (_, args) => {
+      let id,author,comments,content
+
+      [id,author,comments,content] = [args.id,args.author,args.comments,args.content]
+
+      const postsUpdated = {id,author,comments,content}
+
+      posts.push(postsUpdated)
+      return postsUpdated;
     },
+
+
+    // 2. READ ALL THE POSTS
+    // TO READ ALL THE POST IT'S JUST WITH QUERY IN APOLLO SERVER
+
+
+    // 3. READ ONE POST BY ID
+    readpostbyId: (_, args) => {
+      let id,author,comments,content
+
+      [id] = [args.id]
+
+      for (let i=0; i < posts.length; i++) {
+        console.log("df", id.posts[i].id);
+        if (id === posts[i].id) {
+          [author, comments, content] = [args.author, args.comments, args.content]
+          const postsreadId = {id, author, comments, content}
+          console.log("postsreadId", postsreadId)
+          return postsreadId
+        }
+      }
+    },
+
+
+    // 4. UPDATE ONE POST BY ID
+    updatepostbyId: (_, args) => {
+      let id,author,comments,content
+
+      [id] = [args.id]
+      
+      for (let i=0; i < posts.length; i++) {
+        if (id === posts[i].id) {
+          [author, comments, content] = [args.author, args.comments, args.content]
+          const postsUpdated = {id, author, comments, content}
+          console.log("postsUpdated", postsUpdated)
+          return postsUpdated
+        }
+      }
+    },
+
+
+    // 5. DELETE POST BY ONE ID
+
+    deletepostbyId: (_, args) => {
+      let id
+
+      [id] = [args.id]
+  
+      for (let i=0; i < posts.length; i++) {
+        if (id === posts[i].id) {
+          posts.slice(1, 1)
+          console.log("postsUpdated", posts)
+          return posts
+        }
+      }
+  
+    }
+
+
+    // 6. CREATE A COMMENT IN A POST
+
+
+
+
+    // 7. READ  ALL THE COMMENTS FROM A POST
+
+
+    // 8. READ A COMMENT BY ID
+
+
+    // 9. UPDATE A COMMENT BY ID 
+
+    // 10. DELETE POST BY ID
+  }
+
+
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
-
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
